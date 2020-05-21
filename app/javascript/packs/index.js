c6bda40b0dc5f05f83468b6de7fbb3baae2d2a58
Stefan <stefan@edutoit.net>
2020-05-21 15:28:57 +0200

    Update index.js

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,25 +1,67 @@
 var THREE = require('three');
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-scene.add( camera );
-var renderer = new THREE.WebGLRenderer();
+scene.background = new THREE.Color(0xcccccc);
+scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(5, 5, 10);
+// camera.position.set( 400, 200, 0 );
+
+var controls = new OrbitControls(camera, renderer.domElement);
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+controls.screenSpacePanning = false;
+
+controls.minDistance = 1;
+controls.maxDistance = 500;
+
+controls.maxPolarAngle = Math.PI / 2;
+
+var geometry = new THREE.BoxBufferGeometry( 3, 3, 3 );
+var material = new THREE.MeshPhongMaterial({ color: 0x00ff00, flatShading: true });
 var cube = new THREE.Mesh(geometry, material);
+cube.position.setY(1.5);
+cube.updateMatrix();
 scene.add(cube);
 
-camera.position.z = 5;
+camera.lookAt(cube);
+
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+var light = new THREE.DirectionalLight(0x002288);
+light.position.set(- 1, - 1, - 1);
+scene.add(light);
+
+var light = new THREE.AmbientLight(0x222222);
+scene.add(light);
+
+var size = 20;
+var divisions = 20;
+
+var gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+// controls.update();
+
+// camera.position.z = 5;
 
 var animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
+    // cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
+    controls.update();
     renderer.render(scene, camera);
 };
 
